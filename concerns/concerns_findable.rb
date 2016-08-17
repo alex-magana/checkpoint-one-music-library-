ec54a516f2173d9c07203f0528832c446f5cd84c
Alex Magana <alex.magana@andela.com>
2016-08-17 21:36:46 +0300

    Implement the Concerns::Findable module
    Implement the Concerns::Findable module. It exposes functionality to find
    a song and create a song if it doesn't exist.

---
+++ concerns/concerns_findable.rb
@@ -0,0 +1,15 @@
+module Concerns
+	module Findable
+
+		def find_by_name(name)
+			self.all.detect { |object| object.name == name}
+		end
+
+		def find_or_create_by_name(name)
+	        object = find_by_name(name)
+	        object = create(name) unless object
+	        object
+	    end
+	
+	end
+end
