36246f84675ca2444d8dc4518604a6d379dc3c90
Alex Magana <alex.magana@andela.com>
2016-08-17 22:07:58 +0300

    Implement MusicImporter class
    Implement MusicImporter class. It allows for creation of instances of Song
    class when a file path to a directory of songs is passed.

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,22 @@
+require_relative 'Song.rb'
+
+class MusicImporter
+
+	attr_accessor	:path, :file_names
+
+	def initialize(path)
+		@path = path
+		@file_names = []
+	end
+
+	def files
+		Dir::entries(path).select { |_file| @file_names.push(_file) if File.file?"#{path}/#{_file}"}
+		@file_names
+	end
+
+	def import
+		files_all = files
+		files_all.each { |file| Song.new_from_filename(file) }
+	end
+
+end
