43bad36b0112143e440c893ecb2ba5a2a3fe3fa7
andela-amagana <alex.magana@andela.com>
2016-08-19 21:30:00 +0300

    Refactor the code
    Remodel the code to optimize methods.

--- lib/Song.rb
+++ lib/Song.rb
@@ -18,11 +18,12 @@ it to persist immediately.
 
 require_relative 'artist.rb'
 require_relative 'genre.rb'
+require_relative '../concerns/concerns_findable.rb'
 
-class Song
+class Song
+    # extend Concerns::Findable
 
-    attr_accessor   :name 
-    attr_reader     :artist, :genre
+    attr_accessor   :name, :artist, :genre
 
     @@all = []
 
@@ -47,7 +48,7 @@ class Song
 
     def genre=(_genre)
         @genre = _genre
-        @genre.songs.push(self) unless @genre.songs.include?self
+        @genre.songs.push(self) unless @genre.songs.include? self
     end
 
     def save
@@ -59,10 +60,12 @@ class Song
         @@all.clear()
     end
 
+    # use Concerns::Findable to implement find_by_name 
     def self.find_by_name(name)
         @@all.detect { |song| song.name == name}
     end
 
+    # use Concerns::Findable to implement find_or_create_by_name
     def self.find_or_create_by_name(name)
         song = find_by_name(name)
         song = Song.create(name) unless song
@@ -71,13 +74,15 @@ class Song
 
     def self.new_from_filename(file_name)
         song_details = file_name.split(" - ")
+        #use Concerns::Findable to detect artist
         artist = Artist.all.detect {|artist| artist.name == (song_details[0]).strip} || Artist.create((song_details[0]).strip)
+        #use Concerns::Findable to detect genre
         genre = Genre.all.detect { |genre| genre.name == ((song_details[2]).gsub(".mp3", "")).strip } || Genre.create(((song_details[2]).gsub(".mp3", "")).strip)
-        song = Song.new((song_details[1]).strip, artist, genre).save
+        song = Song.new((song_details[1]).strip, artist, genre)
     end
 
     def self.create_from_filename(file_name)
-        new_from_filename(file_name)
+        new_from_filename(file_name).save
     end
 
 end
