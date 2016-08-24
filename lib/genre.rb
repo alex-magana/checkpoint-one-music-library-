2f031cc867db71bd829b06fad41ffa871d3dbbfb
andela-amagana <alex.magana@andela.com>
2016-08-24 12:07:31 +0300

    Correct the indentation
    Use 2 spaces instead of 4 spaces.

--- lib/genre.rb
+++ lib/genre.rb
@@ -16,40 +16,38 @@ an instance using .new but also evokes #save on that instance, forcing
 it to persist immediately.
 =end
 
-require_relative '../concerns/concerns_findable.rb'
-
 class Genre
-
-    extend Concerns::Findable
-
-    attr_accessor   :name, :songs
-
-    @@all = []
-    
-    def initialize(name)
-        @name = name
-        @songs = []
-    end
-
-    def self.create(name)
-        Genre.new(name).save
-    end
-    
-    def self.all
-        @@all
-    end
-
-    def save
-        @@all.push(self)
-        self
-    end
-
-    def self.destroy_all
-        @@all.clear()
-    end
-
-    def artists
-        @songs.map { |song| song.artist unless song.artist.nil? }.uniq
-    end
-
+  
+  extend Concerns::Findable
+  
+  attr_accessor   :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.create(name)
+    Genre.new(name).save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all.push(self)
+    self
+  end
+  
+  def self.destroy_all
+    @@all.clear()
+  end
+  
+  def artists
+    @songs.map { |song| song.artist unless song.artist.nil? }.uniq
+  end
+  
 end
