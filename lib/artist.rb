8949ee73954a6dafc1b1690be9179fb826045cab
andela-amagana <alex.magana@andela.com>
2016-08-24 12:03:07 +0300

    Correct the indentation
    Use 2 spaces instead of 4 spaces.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,45 +16,43 @@ an instance using .new but also evokes #save on that instance, forcing
 it to persist immediately.
 =end
 
-require_relative '../concerns/concerns_findable.rb'
-
 class Artist
 
-    extend Concerns::Findable
-
-    attr_accessor   :name, :songs
-
-    @@all = []
-    
-    def initialize(name)
-        @name = name
-        @songs = []
-    end
-
-    def self.create(name)
-        Artist.new(name).save
-    end
-    
-    def self.all
-        @@all
-    end
-
-    def save
-        @@all.push(self)
-        self
-    end
-
-    def self.destroy_all
-        @@all.clear()
-    end
-    
-    def add_song(song)
-        @songs.push(song) unless @songs.include?song
-        song.artist = self unless song.artist == self
-    end
-
-    def genres
-       @songs.map { |song| song.genre unless song.genre.nil? }.uniq
-    end
-
+  extend Concerns::Findable
+  
+  attr_accessor   :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.create(name)
+    Artist.new(name).save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all.push(self)
+    self
+  end
+  
+  def self.destroy_all
+    @@all.clear()
+  end
+  
+  def add_song(song)
+    @songs.push(song) unless @songs.include?song
+    song.artist = self unless song.artist == self
+  end
+  
+  def genres
+    @songs.map { |song| song.genre unless song.genre.nil? }.uniq
+  end
+  
 end
