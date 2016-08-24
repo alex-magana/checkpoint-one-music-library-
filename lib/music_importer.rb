38a6f95630e4aef05ecf84ebb1555de948e16061
andela-amagana <alex.magana@andela.com>
2016-08-24 12:13:14 +0300

    Correct indentation and filter path files
    Use 2 spaces instead of 4 spaces. Use method glob to return .mp3
    files from the path provided.
    
    Combine command_avaluate and command_execute. Include help method
    to suport wrong user input. Implement check for song number validity
    for command play song.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,19 +1,27 @@
+=begin
+Accepts a file path of mp3 files. A #files method that will 
+return all of the filenames. The .import method imports 
+all the files from the library and creates the Song objects.
+=end
+
 require_relative 'song.rb'
 
 class MusicImporter
-
-    attr_accessor   :path, :file_names
-
-    def initialize(path)
-        @path = path
+  
+  attr_accessor   :path, :file_names
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    Dir.chdir(path) do
+      Dir.glob("*.mp3")
     end
-
-    def files
-        Dir::entries(path).select { |_file| File.file?"#{path}/#{_file}"}
-    end
-
-    def import
-        files.each { |file| Song.create_from_filename(file) }
-    end
-
+  end
+  
+  def import
+    files.each { |file| Song.create_from_filename(file) }
+  end
+  
 end
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,88 +9,111 @@ input. Check out the tests for specifics
 require_relative 'music_importer.rb'
 
 class MusicLibraryController
-
-    attr_accessor   :path, :music_importer
-
-    COMMANDS = {"list songs" => :list_songs, 
-        "list artists" => :list_artists, 
-        "list genres" => :list_genres, 
-        "play song" => :play_song,
-        "list artist" => :list_artist,
-        "list genre" => :list_genre}
-
-    PROMPT = "music_library>"
-
-    def initialize(path = "./db/mp3s")
-        @path = path
-        MusicImporter.new(path).import
+  
+  attr_accessor   :path
+  
+  COMMANDS = {"list songs" => :list_songs, 
+    "list artists" => :list_artists, 
+    "list genres" => :list_genres, 
+    "play song" => :play_song,
+    "list artist" => :list_artist,
+    "list genre" => :list_genre,
+    "help" => :help}
+  
+  PROMPT = "music_library>"
+  
+  def initialize(path = "./db/mp3s")
+    @path = path
+    MusicImporter.new(path).import
+  end
+  
+  def call
+    introduction
+    
+    while true
+      print PROMPT
+      user_input = self.send(:gets).chomp.strip.downcase
+      break if user_input == "exit" 
+      self.command_execute(user_input)
     end
-
-    def call
-        introduction
-        
-        while true
-            print PROMPT
-            user_input = self.send(:gets).chomp
-            break if user_input == "exit" 
-            self.command_execute(user_input)
-        end
-        
+    
+  end
+  
+  def introduction    
+    print "Music Library\n\n\t\tUsage:\n" \
+      "\tlist songs\t: List all songs.\n" \
+      "\tlist artists\t: List all artists.\n" \
+      "\tlist genres\t: List all genres.\n" \
+      "\tplay song\t: Play a song.\n" \
+      "\tlist artist\t: List the artist.\n" \
+      "\tlist genre\t: List the genre.\n" \
+      "\texit\t\t: Quit the program.\n" \
+      "\thelp\t\t: View available commands.\n"
+  end
+  
+  def command_execute(command_name)
+    if COMMANDS.include? command_name
+      self.send(COMMANDS[command_name])
+    else
+      puts "Invalid command.\n"
+      help
     end
-
-    def introduction    
-        print "Music Library\n\n\t\tUsage:\n" \
-        "\tlist songs\t: List all songs.\n" \
-        "\tlist artists\t: List all artists.\n" \
-        "\tlist genres\t: List all genres.\n" \
-        "\tplay song\t: Play a song.\n" \
-        "\tlist artist\t: List the artist.\n" \
-        "\tlist genre\t: List the genre.\n" \
-        "\texit\t: Quit the program.\n"
+  end
+  
+  def list_songs
+    Song.all.each_with_index do |song, i|
+      puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
-
-    def command_execute(command_name)
-        self.send(COMMANDS[command_name])
+  end
+  
+  def list_artists
+    Artist.all.each_with_index do |artist, i|
+      puts "#{i + 1}. #{artist.name}"
     end
-
-    def list_songs
-        Song.all.each_with_index do |song, i|
-            puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-        end
+  end
+  
+  def list_genres
+    Genre.all.each_with_index do |genre, i|
+      puts "#{i + 1}. #{genre.name}"
     end
-
-    def list_artists
-        Artist.all.each_with_index do |artist, i|
-            puts "#{i + 1}. #{artist.name}"
-        end
+  end
+  
+  def play_song
+    puts "Enter the number of the song to play."
+    song_number = self.send(:gets).chomp.to_i
+    match = /[a-zA-Z]+/.match(song_number.to_s)
+    if (song_number <= Song.all.size) && (match == nil)
+      song = Song.all[song_number - 1]
+      puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    else
+      puts "The song you requested does not exist. Enter numbers 1 through #{Song.all.size}."
     end
-
-    def list_genres
-        Genre.all.each_with_index do |genre, i|
-            puts "#{i + 1}. #{genre.name}"
-        end
+  end
+  
+  def list_artist
+    puts "Enter the name of the artist."
+    artist_name = self.send(:gets).chomp
+    artist = Artist.find_by_name(artist_name)
+    if artist
+      artist.songs.each { |song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}" }
+    else
+      "The artist you requested does not exist. Use command 'list artists' to view available artists."
     end
-
-    def play_song
-        puts "Enter the number of the song to play."
-        song_number = self.send(:gets).chomp.to_i
-        song = Song.all[song_number - 1]
-        puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+  
+  def list_genre
+    puts "Enter the genre whose songs you would like listed."
+    genre_name = self.send(:gets).chomp
+    genre = Genre.find_by_name(genre_name)
+    if genre
+      genre.songs.each { |song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}" }
+    else
+      "The genre you requested does not exist. Use command 'list genres' to view available artists."
     end
-
-    def list_artist
-        puts "Enter the name of the artist."
-        artist_name = self.send(:gets).chomp
-        artist = Artist.find_by_name(artist_name)
-        artist.songs.each { |song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}" }
-        
-    end
-
-    def list_genre
-        puts "Enter the genre whose songs you would like listed."
-        genre_name = self.send(:gets).chomp
-        genre = Genre.find_by_name(genre_name)
-        genre.songs.each { |song| puts "#{song.artist.name} - #{song.name} - #{song.genre.name}" }
-    end
-
+  end
+  
+  def help
+    introduction
+  end
+  
 end
