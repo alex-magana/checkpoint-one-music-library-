469cd32432134d2cfefe4205ad8ffd7398300d95
andela-amagana <alex.magana@andela.com>
2016-08-26 15:45:12 +0300

    Use modules to implement shared methods

--- lib/song.rb
+++ lib/song.rb
@@ -1,63 +1,46 @@
-=begin 
-Accepts a name upon initialization and set that property correctly. The name 
-property should be readable and writeable by the object.
+# Accepts a name upon initialization and set that property correctly. The name 
+# property should be readable and writeable by the object.
 
-Contains a class variable @@all that is set to an empty array and is 
-prepared to store all saved instances. This class variable should be 
-accessible via the class method .all.
+# Contains a class variable @@all that is set to an empty array and is 
+# prepared to store all saved instances. This class variable should be 
+# accessible via the class method .all.
 
-It responds to a #save method that adds the instance itself into the 
-class variable @@all.
+# It responds to a #save method that adds the instance itself into the 
+# class variable @@all.
 
-The class empties it's @@all array via a class method .destroy_all.
+# The class empties it's @@all array via a class method .destroy_all.
 
-Implements a custom constructor .create that instantiates 
-an instance using .new but also evokes #save on that instance, forcing 
-it to persist immediately.
-=end
+# Implements a custom constructor .create that instantiates 
+# an instance using .new but also evokes #save on that instance, forcing 
+# it to persist immediately.
 
 class Song
-  
+
   extend Concerns::Findable
-  
+  extend Concerns::Generic
+  include Concerns::GenericInstance
+
   attr_accessor   :name
   attr_reader     :artist, :genre
-  
+
   @@all = []
-  
+
   def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist
     self.genre = genre if genre
   end
-  
-  def self.create(name)
-    Song.new(name).save
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def artist=(_artist)
-    @artist = _artist
+
+  def artist=(artist_instance)
+    @artist = artist_instance
     @artist.add_song(self)
   end
-  
-  def genre=(_genre)
-    @genre = _genre
+
+  def genre=(genre_instance)
+    @genre = genre_instance
     @genre.songs.push(self) unless @genre.songs.include? self
   end
-  
-  def save
-    @@all.push(self)
-    self
-  end
-  
-  def self.destroy_all
-    @@all.clear()
-  end
-  
+
   def self.new_from_filename(file_name)
     song_details = file_name.gsub(".mp3", "").split(" - ")
     if song_details.size == 3
@@ -67,11 +50,11 @@ class Song
     else
       song = Song.new((song_details[0]).strip)
     end
-    
   end
-  
+
   def self.create_from_filename(file_name)
     new_from_filename(file_name).save
   end
-  
+
 end
+
