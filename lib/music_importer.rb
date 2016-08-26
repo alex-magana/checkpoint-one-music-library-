dad737f2024dc323427d236d16837ce076dc7f01
andela-amagana <alex.magana@andela.com>
2016-08-26 15:52:19 +0300

    Remove unused variables

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,27 +1,23 @@
-=begin
-Accepts a file path of mp3 files. A #files method that will 
-return all of the filenames. The .import method imports 
-all the files from the library and creates the Song objects.
-=end
-
-require_relative 'song.rb'
+# Accepts a file path of mp3 files. A #files method that will 
+# return all of the filenames. The .import method imports 
+# all the files from the library and creates the Song objects.
 
 class MusicImporter
-  
-  attr_accessor   :path, :file_names
-  
+
+  attr_accessor   :path
+
   def initialize(path)
     @path = path
   end
-  
+
   def files
     Dir.chdir(path) do
       Dir.glob("*.mp3")
     end
   end
-  
+
   def import
     files.each { |file| Song.create_from_filename(file) }
   end
-  
+
 end
